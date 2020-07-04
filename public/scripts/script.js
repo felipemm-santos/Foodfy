1e8a3422cca238de450d198763be54e11147a502
Felipe Morais Mariano dos Santos <tfelipesantos99s@gmail.com>
2020-07-04 08:10:54 -0300

    Configure site to use templates

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,19 +1,8 @@
 const cards = document.querySelectorAll('.card');
 
-const modalOverlay = document.querySelector('.modal-overlay');
-
-const modalContent = modalOverlay.querySelector('.modal-content');
-
 for (const card of cards) {
   card.addEventListener('click', () => {
-    modalOverlay.classList.remove('hidden');
-    modalContent.innerHTML = card.innerHTML;
+    
   });
 }
 
-// Close modal
-
-document.querySelector('.close-modal').addEventListener('click', () => {
-  modalOverlay.classList.add('hidden');
-  modalContent = ''
-});
--- server.js
+++ server.js
@@ -18,20 +18,32 @@ nunjucks.configure('views', {
 });
 
 server.get('/', (req, res) => {
-  return res.render('home'});
+  return res.render('home', {
+    recipes: recipes.slice(0, 6),
+  });
+});
+
+server.get('/about', (req, res) => {
+  return res.render('about-page');
 });
 
 server.get('/recipes', (req, res) => {
-  return res.render('recipes');
+  return res.render('recipes-page', { recipes });
 });
 
-// server.get('/recipes/:index', (req, res) => {
-//     const id = req.params.id;
+server.get('/recipes/:index', (req, res) => {
+  const recipeIndex = req.params.index;
 
-//   const recipe = recipe.find((recipe) => recipe.id == id);
+  const recipe = recipes[recipeIndex]
 
-//   if (!recipe) {
-//     return res.status(404).render('not-found');
-//   }
-//     return res.render('recipes' , { ecipe });
-//   });
+  if (!recipe) {
+    return res.status(404).render('not-found');
+  }
+  return res.render('recipe', { recipe });
+});
+
+server.use((req, res) => res.status(404).render('not-found'));
+
+server.listen(5000, () => {
+  console.log('server is running');
+});
