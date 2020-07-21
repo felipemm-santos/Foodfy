48afe74192e0e3b5192099f4f6e91a78b44e9ab6
Felipe Morais Mariano dos Santos <tfelipesantos99s@gmail.com>
2020-07-21 08:04:27 -0300

    Start admin section

---
+++ controllers/admin.js
@@ -0,0 +1,6 @@
+const data = require('../data.json')
+const recipes = data.recipes
+
+exports.index = (req , res) => {
+    return res.render('admin/index')
+}
--- routes.js
+++ routes.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const routes = express.Router();
 const website = require('./controllers/website')
+const admin  = require('./controllers/admin')
 
 routes.get('/', (req, res) => res.redirect('/website'));
-routes.get('/website', website.home);
+routes.get('/website', website.index);
 routes.get('/about', website.about);
 routes.get('/recipes', website.recipes);
 routes.get('/recipes/:index', website.show);
+
+// Admin
+
+routes.get("/admin", (req, res) => res.redirect("/admin/recipes"));
+routes.get("/admin/recipes", admin.index); // Mostrar a lista de receitas
 
 
 routes.use((req, res) => res.status(404).render('not-found'));
