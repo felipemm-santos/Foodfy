cb9dbfec028342aa2ea02f427b32e2311c62c9a3
Felipe Morais Mariano dos Santos <tfelipesantos99s@gmail.com>
2020-07-23 07:18:17 -0300

    Fix some bugs in website section

--- public/scripts/recipes-website.js
+++ public/scripts/recipes-website.js
@@ -1,11 +1,9 @@
 const cards = document.querySelectorAll('.card');
 
 for (let cardIndex = 0; cardIndex < cards.length; cardIndex++) {
-  const card = cards[cardIndex]
-
-  card.
+  const card = cards[cardIndex]  
 
   card.addEventListener('click', () => {
-    window.location.href = `website/recipes/${cardIndex}`;    
+    window.location.href = `/website/recipes/${cardIndex}`;    
   });  
 }
